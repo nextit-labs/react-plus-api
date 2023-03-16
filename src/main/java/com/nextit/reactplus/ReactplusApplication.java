4d76dd49d065ec13ef1c33943046209d56cdc15b
next-it <nextit.labs@gmail.com>
2023-03-16 16:26:57 +0900

    Changes

---
+++ src/main/java/com/nextit/reactplus/ReactplusApplication.java
@@ -0,0 +1,13 @@
+package com.nextit.reactplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nextit/reactplus/ReactplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextit.reactplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
